4d826e7080b4922d53a531230bd70a5940b5535f
Spcie <m15188159686@163.com>
2016-11-24 23:02:15 +0800

    chart add
    use canves make a chart

--- canvas/pid.js
+++ canvas/pid.js
@@ -1,4 +1,68 @@
 
+var AxislengthX = 450;
+var AxislengthY = 350;
+var OriginX = 40; 
+var OriginY = 500;
+	
+function DrawChart(){
+	var can = document.getElementById("canss");
+	var cxt = can.getContext("2d");
+	
+	var AxislengthX = window.AxislengthX;
+	var AxislengthY = window.AxislengthY;
+	var OriginX = window.OriginX; 
+	var OriginY = window.OriginY;
+	
+	cxt.lineWidth = 2;
+	cxt.strokeStyle = "#000000";
+	cxt.beginPath();
+	//Y Axis
+	cxt.moveTo(OriginX,OriginY);
+	cxt.lineTo(OriginX,OriginY-AxislengthY);
+	cxt.lineTo(OriginX-9,OriginY-AxislengthY+12);
+	cxt.moveTo(OriginX,OriginY-AxislengthY);
+	cxt.lineTo(OriginX+9,OriginY-AxislengthY+12);
+	//X Axis
+	cxt.moveTo(OriginX,OriginY);
+	cxt.lineTo(OriginX+AxislengthX,OriginY);
+	cxt.lineTo(OriginX+AxislengthX-12,OriginY+9);
+	cxt.moveTo(OriginX+AxislengthX,OriginY);
+	cxt.lineTo(OriginX+AxislengthX-12,OriginY-9);
+	//Scale Y
+	cxt.moveTo(OriginX,OriginY-100);
+	cxt.lineTo(OriginX+8,OriginY-100);
+	cxt.moveTo(OriginX,OriginY-200);
+	cxt.lineTo(OriginX+8,OriginY-200);
+	cxt.moveTo(OriginX,OriginY-300);
+	cxt.lineTo(OriginX+8,OriginY-300);
+	//cxt.moveTo(OriginX,OriginY-400);
+	//cxt.lineTo(OriginX+8,OriginY-400);
+	
+	//Scale X
+	cxt.moveTo(OriginX+100,OriginY);
+	cxt.lineTo(OriginX+100,OriginY-8);
+	cxt.moveTo(OriginX+200,OriginY);
+	cxt.lineTo(OriginX+200,OriginY-8);
+	cxt.moveTo(OriginX+300,OriginY);
+	cxt.lineTo(OriginX+300,OriginY-8);
+	cxt.moveTo(OriginX+400,OriginY);
+	cxt.lineTo(OriginX+400,OriginY-8);
+	
+	cxt.stroke();
+	cxt.closePath();
+	
+	pint();
+	
+}
+function Transform(Rx,Ry){
+	var x,y;
+	
+	x = window.OriginX + Rx;
+	y = window.OriginY - Ry;
+	
+	
+	return {"x":x,"y":y};
+}
 function pint(){
 
 	var can = document.getElementById("canss");
@@ -9,14 +73,19 @@ function pint(){
 	cxt.lineWidth = 1;
 	cxt.strokeStyle = "#AA394C";
 	cxt.beginPath();
+	var a;
+	a = Transform(0,0);
+	cxt.moveTo(a.x,a.y);
+	a = Transform(300,400);
+	cxt.lineTo(a.x,a.y);
 	var value = 0;
-	for(var i=0;i<300;i++)
+	for(var i=0;i<600;i++)
 	{
 		
 		cxt.moveTo(i,value);
 		value = PID(value,600);
 		value = 300;
-		cxt.lineTo(i+1,value);
+		cxt.lineTo(i,value);
 		cxt.stroke();
 		//ctx.closePath();
 	}
